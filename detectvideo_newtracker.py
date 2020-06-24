a37eb45c0bc74e8f8db7ecc6e642b5f418cdc2eb
molimat <matheusmolin@poli.ufrj.br>
2020-06-24 09:58:48 +0200

    bad position in def intersect

--- detectvideo_newtracker.py
+++ detectvideo_newtracker.py
@@ -18,7 +18,12 @@ flags.DEFINE_boolean('tiny', False, 'yolo or yolo-tiny')
 flags.DEFINE_string('model', 'yolov4', 'yolov3 or yolov4')
 flags.DEFINE_string('video', './data/road.avi', 'path to input video')
 
+def intersect(A,B,C,D):
+	return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
 
+def ccw(A,B,C):
+	return (C[1]-A[1]) * (B[0]-A[0]) > (B[1]-A[1]) * (C[0]-A[0])
+    
 def main(_argv):
     if FLAGS.tiny:
         STRIDES = np.array(cfg.YOLO.STRIDES_TINY)
@@ -204,8 +209,3 @@ if __name__ == '__main__':
     except SystemExit:
         pass
 
-def intersect(A,B,C,D):
-	return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
-
-def ccw(A,B,C):
-	return (C[1]-A[1]) * (B[0]-A[0]) > (B[1]-A[1]) * (C[0]-A[0])
